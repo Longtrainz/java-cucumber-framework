d7ff8e48fb688225402861a888d5d035fa479d47
Vlad <chinazaro@gmail.com>
2019-11-03 15:42:14 +0200

    Cucumber first steps

---
+++ src/test/java/runner/MainRunner.java
@@ -0,0 +1,18 @@
+package runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/java/features"},
+        glue = {"steps"},
+        monochrome = true,
+        tags = {},
+        plugin = {"pretty", " ","json:target/cucumber.json", "com.cucumber.listener.ExtentCucumberFormatter: target/report.html"}
+)
+public class MainRunner {
+
+
+}
