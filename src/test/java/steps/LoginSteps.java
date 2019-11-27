4c9bdea1598f48011cc9d1433cfb75c53a246f90
vladyslav.bukach <vladyslav.bukach@globallogic.com>
2019-11-27 16:23:42 +0200

    Add stackoverflow tests

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,38 +1,65 @@
 package steps;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginSteps {
+    WebDriver driver;
+
+    @Before()
+    public void setup() {
+        System.setProperty("webdriver.gecko.driver", "src/test/java/resources/geckodriver.exe");
+        this.driver = new FirefoxDriver();
+        this.driver.manage().window().maximize();
+        this.driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+    }
+
+    @After()
+    public void teardown() {
+      driver.quit();
+    }
+
+
     @Given("^User navigates to stackoverflow website$")
     public void user_navigates_to_stackoverflow_website() throws Throwable {
-        System.out.println("User navigates to stackoverflow website");
+        driver.get("https://stackoverflow.com/");
     }
 
     @Given("^User clicks on the login button on homepage$")
     public void user_clicks_on_the_login_button_on_homepage() throws Throwable {
-        System.out.println("User navigates to stackoverflow website");
+        driver.findElement(By.xpath("//a[text()='Log in']")).click();
     }
 
     @Given("^User enters a valid username$")
     public void user_enters_a_valid_username() throws Throwable {
-        System.out.println("User navigates to stackoverflow website");
+        driver.findElement(By.id("email")).sendKeys("testedflow@gmail.com");
     }
 
     @Given("^User enters a valid password$")
     public void user_enters_a_valid_password() throws Throwable {
-        System.out.println("User navigates to stackoverflow website");
+        driver.findElement(By.id("password")).sendKeys("123qwe123");
     }
 
     @When("^User clicks on the login button$")
     public void user_clicks_on_the_login_button() throws Throwable {
-        System.out.println("User navigates to stackoverflow website");
+        driver.findElement(By.id("submit-button")).click();
     }
 
     @Then("^User should be taken to the successful login page$")
     public void user_should_be_taken_to_the_successful_login_page() throws Throwable {
-        System.out.println("User navigates to stackoverflow website");
+        WebElement askQuestionButton = driver.findElement(By.xpath("//a[contains(text(),'Ask Question')]"));
+        Assert.assertTrue(askQuestionButton.isDisplayed());
+        Assert.assertEquals(askQuestionButton.getText(), "Ask Question");
     }
 
 
